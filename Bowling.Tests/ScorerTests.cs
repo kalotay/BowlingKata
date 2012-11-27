2bfd8bab9a6160235eea75642fb0eb32df2a7847
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-11-27 12:32:24 +0000

    scorer tests

---
+++ Bowling.Tests/ScorerTests.cs
@@ -0,0 +1,20 @@
+﻿using Bowling.DefaultImplementation;
+using Bowling.Interface;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Bowling.Tests
+{
+    [TestFixture]
+    public class ScorerTests
+    {
+        [Test]
+        public void InitialScoreIs0()
+        {
+            var multiplier = Substitute.For<IBonusMultiplier>();
+            var scorer = new TenPinScorer(multiplier);
+
+            Assert.That(scorer.Score, Is.EqualTo(0));
+        }
+    }
+}
