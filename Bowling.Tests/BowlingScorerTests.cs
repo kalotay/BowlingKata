15d11442595be1f36d71e2e44587a553ee6aaa8f
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-11 14:36:49 +0000

    bowling scorer done

---
+++ Bowling.Tests/BowlingScorerTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Bowling.Tests
+{
+    public class BowlingScorerTests
+    {
+        private BowlingScorer _scorer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _scorer = new BowlingScorer();
+        }
+
+        [Test]
+        public void InitialScoreIsZero()
+        {
+            Assert.That(_scorer.Score, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void InitiallyIncomplete()
+        {
+            Assert.That(_scorer.IsComplete, Is.False);
+        }
+
+        [TestCase(new[] { 1, 1, 1, 1 }, false)]
+        [TestCase(new[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, true)]
+        [TestCase(new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, true)]
+        public void TestCompletion(int[] rolls, bool completion)
+        {
+            foreach (var roll in rolls)
+            {
+                _scorer.Register(roll);
+            }
+
+            Assert.That(_scorer.IsComplete, Is.EqualTo(completion));
+        }
+    }
+}
---
+++ Bowling/BowlingScorer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bowling
+{
+    public class BowlingScorer: IScorer<int>
+    {
+        private readonly List<Frame> _frames;
+        private int _sinceLastGen;
+
+        public BowlingScorer()
+        {
+            _frames = new List<Frame>();
+            _sinceLastGen = 0;
+        }
+
+        public IComparable<int> Score
+        {
+            get { return _frames.Sum(f => (int) f.Score); }
+        }
+
+        public void Register(int move)
+        {
+            if (IsFrameToBeGenerated(move)) _frames.Add(new Frame());
+
+            if (IsComplete) throw new CompletedException();
+
+            foreach (var frame in _frames.Where(f => !f.IsComplete))
+            {
+                frame.Register(move);
+            }
+
+            _sinceLastGen += 1;
+        }
+
+        public bool IsComplete
+        {
+            get { return _frames.Count >= 10 && _frames.All(f => f.IsComplete); }
+        }
+
+        private bool IsFrameToBeGenerated(int roll)
+        {
+            var isFrameToBeGenerated = (_frames.Count < 10) && (roll == 10 || (_sinceLastGen%2) == 0);
+            if (isFrameToBeGenerated) _sinceLastGen = 0;
+
+            return isFrameToBeGenerated;
+        }
+    }
+}
