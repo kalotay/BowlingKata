ec626737f30f94c6bc6c999be2f283ec52c5231e
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-13 13:47:05 +0000

    added more testcases

--- Bowling.Tests/BowlingScorerTests.cs
+++ Bowling.Tests/BowlingScorerTests.cs
@@ -28,10 +28,12 @@ namespace Bowling.Tests
             Assert.That(_scorer.IsComplete, Is.False);
         }
 
-        [TestCase("1111", false)]
-        [TestCase("xxxxxxxxxxxx", true)]
-        [TestCase("11111111111111111111", true)]
-        public void TestCompletion(string rolls, bool completion)
+        [TestCase("1111", false, 4)]
+        [TestCase("xxxxxxxxxxxx", true, 300)]
+        [TestCase("11111111111111111111", true, 20)]
+        [TestCase("00,00,00,00,00,00,00,00,00,00", true, 0)]
+        [TestCase("10,00,00,00,00,00,00,00,00,00", true ,1)]
+        public void TestCases(string rolls, bool completion, int score)
         {
             var filtered = _filter.Filter(rolls);
             foreach (var mapped in filtered.Select(roll => _mapper.Map(roll)))
@@ -40,6 +42,7 @@ namespace Bowling.Tests
             }
 
             Assert.That(_scorer.IsComplete, Is.EqualTo(completion));
+            Assert.That(_scorer.Score, Is.EqualTo(score));
         }
     }
 }
