c5b3c8f265509f64def3fc423e75ab019bca0fa3
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-13 10:20:14 +0000

    maps spares

--- Bowling.Tests/RollMapperTests.cs
+++ Bowling.Tests/RollMapperTests.cs
@@ -37,5 +37,23 @@ namespace Bowling.Tests
 
             Assert.That(actual, Is.EqualTo(10));
         }
+
+        [TestCase('0')]
+        [TestCase('1')]
+        [TestCase('2')]
+        [TestCase('3')]
+        [TestCase('4')]
+        [TestCase('5')]
+        [TestCase('6')]
+        [TestCase('7')]
+        [TestCase('8')]
+        [TestCase('9')]
+        public void MapSlashToTenMinusPreviousRoll(char previous)
+        {
+            var previousInt = _mapper.Map(previous);
+            var actual = _mapper.Map('/');
+
+            Assert.That(actual, Is.EqualTo(10 - previousInt));
+        }
     }
 }
--- Bowling/RollMapper.cs
+++ Bowling/RollMapper.cs
@@ -1,15 +1,38 @@
-﻿namespace Bowling
+﻿using System;
+
+namespace Bowling
 {
     public class RollMapper: IRollMapper<char>
     {
+        private int _previous;
+
+        public RollMapper()
+        {
+            _previous = 0;
+        }
+
         public int Map(char input)
         {
+            int result;
+
             if (char.ToLowerInvariant(input) == 'x')
             {
-                return 10;
+                result = 10;
+            }
+            else if (input == '/')
+            {
+                result = 10 - _previous;
+            }
+            else
+            {
+                if (!int.TryParse(input.ToString(), out result))
+                {
+                    throw new ArgumentException("Not a valid roll");
+                }
             }
 
-            return int.Parse(input.ToString());
+            _previous = result;
+            return result;
         }
     }
 }
