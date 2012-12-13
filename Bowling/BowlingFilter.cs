fdf4f059fd0eb74e0ef67a0bfdc7c8b2070fa517
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-13 13:38:55 +0000

    added regex filter

---
+++ Bowling/BowlingFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Bowling
+{
+    public class BowlingFilter
+    {
+        private static readonly Regex RegularExpression = new Regex(@"[^0-9xX\/]");
+
+        public string Filter(string input)
+        {
+            return RegularExpression.Replace(input, string.Empty);
+        }
+    }
+}
