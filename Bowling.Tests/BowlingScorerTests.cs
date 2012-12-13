b56b81a138071873e2f41cffbf13fbe246191e49
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-13 14:16:33 +0000

    added test cases and fixed factory bug

--- Bowling.Tests/BowlingScorerTests.cs
+++ Bowling.Tests/BowlingScorerTests.cs
@@ -32,7 +32,15 @@ namespace Bowling.Tests
         [TestCase("xxxxxxxxxxxx", true, 300)]
         [TestCase("11111111111111111111", true, 20)]
         [TestCase("00,00,00,00,00,00,00,00,00,00", true, 0)]
-        [TestCase("10,00,00,00,00,00,00,00,00,00", true ,1)]
+        [TestCase("10,00,00,00,00,00,00,00,00,00", true, 1)]
+        [TestCase("10,10,10,10,10,10,10,10,10,10", true, 10)]
+        [TestCase("7/,81,32,31,33,9/,53,23,52,33", true, 83)]
+        [TestCase("x,36,32,31,33,9/,53,23,52,33", true, 84)]
+        [TestCase("x,36,32,31,33,9/,53,23,52,9/7", true, 95)]
+        [TestCase("00,x,x,x,x,x,x,x,x,xxx", true, 270)]
+        [TestCase("9/,9/,9/,9/,9/,9/,9/,9/,9/,90", true, 180)]
+        [TestCase("7/,42,32,31,33,9/,53,23,52,33", true, 76)]
+        [TestCase("x,7/,72,9/,x,x,x,23,6/,7/3", true, 168)]
         public void TestCases(string rolls, bool completion, int score)
         {
             var filtered = _filter.Filter(rolls);
