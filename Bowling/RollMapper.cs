800dcf4c0edc50b92b3cca9032d146b0ba607521
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-13 10:00:25 +0000

    Mapper started

---
+++ Bowling/RollMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Bowling
+{
+    public class RollMapper: IRollMapper<char>
+    {
+        public int Map(char input)
+        {
+            return int.Parse(input.ToString());
+        }
+    }
+}
